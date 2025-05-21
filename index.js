f78b8a56f71958be6db26242034c2f861b69e0f4
Rubaid07 <rubaidindu@gmail.com>
2025-05-21 06:15:15 +0600

    post task data in db

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ynxyt70.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  } 
+});
+
+async function run() {
+  try {
+
+    // await client.connect();
+    const tasksCollection = client.db('TaskTideDB').collection('tasks')
+    app.post('/tasks', async(req, res)=>{
+      const newTask = req.body
+      console.log(newTask);
+      const result = await tasksCollection.insertOne(newTask)
+      res.send(result)
+    })
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send("Task server")
+})
+app.listen(port, () => {
+  console.log(`Task server is running on port ${port}`);
+})
